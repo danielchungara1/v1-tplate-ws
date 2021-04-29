c8398c3de7178664ec7878589638523bf5dec5ae
daniel <danielchungara1@gmail.com>
2021-04-29 16:40:37 -0300

    Refactor Jwt. #4

--- src/main/java/com/tplate/security/jwt/JwtExceptionHandlerFilter.java
+++ src/main/java/com/tplate/security/jwt/JwtExceptionHandlerFilter.java
@@ -3,9 +3,13 @@ package com.tplate.security.jwt;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tplate.layers.access.dtos.ResponseSimpleDto;
 import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -22,6 +26,9 @@ public class JwtExceptionHandlerFilter extends OncePerRequestFilter {
     @Autowired
     ObjectMapper objectMapper;
 
+    private static final String CONTENT_TYPE_JSON = "application/json";
+
+
     @Override
     public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         try {
@@ -31,11 +38,41 @@ public class JwtExceptionHandlerFilter extends OncePerRequestFilter {
         catch (ExpiredJwtException e){
             log.error("JWT exception. {}", e.getClass().getCanonicalName());
 
-            response.setContentType("application/json");
+            response.setContentType(CONTENT_TYPE_JSON);
             response.setStatus(HttpStatus.BAD_REQUEST.value());
 
             ResponseSimpleDto responseSimpleDto = ResponseSimpleDto.builder()
-                    .message("Session finished.")
+                    .message("Token expired.")
+                    .details("Try logging again.")
+                    .build();
+
+            response.getWriter().write(objectMapper.writeValueAsString(responseSimpleDto));
+
+        }
+        catch (UsernameNotFoundException e){
+            log.error("JWT exception. {}", e.getClass().getCanonicalName());
+
+            response.setContentType(CONTENT_TYPE_JSON);
+            response.setStatus(HttpStatus.BAD_REQUEST.value());
+
+            ResponseSimpleDto responseSimpleDto = ResponseSimpleDto.builder()
+                    .message("Username not exist.")
+                    .details("Try logging again.")
+                    .build();
+
+            response.getWriter().write(objectMapper.writeValueAsString(responseSimpleDto));
+
+        }
+        catch (UnsupportedJwtException | MalformedJwtException
+                | SignatureException | IllegalArgumentException e) {
+
+            log.error("JWT exception. {}", e.getClass().getCanonicalName());
+
+            response.setContentType(CONTENT_TYPE_JSON);
+            response.setStatus(HttpStatus.BAD_REQUEST.value());
+
+            ResponseSimpleDto responseSimpleDto = ResponseSimpleDto.builder()
+                    .message("Invalid token.")
                     .details(e.getMessage())
                     .build();
 
@@ -46,7 +83,7 @@ public class JwtExceptionHandlerFilter extends OncePerRequestFilter {
 
             e.printStackTrace();
 
-            log.error("JWT exception. {}", e.getClass().getCanonicalName());
+            log.error("Unexpected exception. {}", e.getClass().getCanonicalName());
 
             response.setContentType("application/json");
             response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
