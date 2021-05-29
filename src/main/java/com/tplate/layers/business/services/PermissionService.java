156c8603ed560a15e88e15a38a0e4b5a032c5f38
daniel <danielchungara1@gmail.com>
2021-05-29 15:57:44 -0300

    Searching by text on Permission. #2

--- src/main/java/com/tplate/layers/business/services/PermissionService.java
+++ src/main/java/com/tplate/layers/business/services/PermissionService.java
@@ -1,10 +1,13 @@
 package com.tplate.layers.business.services;
 
+import com.tplate.layers.access.filters.SearchText;
 import com.tplate.layers.business.exceptions.permission.PermissionNotExistException;
 import com.tplate.layers.persistence.models.Permission;
 import com.tplate.layers.persistence.repositories.PermissionRepository;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,22 +18,26 @@ import java.util.List;
 public class PermissionService {
 
     @Autowired
-    PermissionRepository permissionRepository;
+    PermissionRepository repository;
 
     @Transactional
     public List<Permission> findAll() {
-        return this.permissionRepository.findAll();
+        return this.repository.findAll();
     }
 
     @Transactional
     public Permission getModelById(Long id) throws PermissionNotExistException {
 
-        if (!this.permissionRepository.existsById(id)) {
+        if (!this.repository.existsById(id)) {
             PermissionNotExistException.throwsException(id);
         }
 
-        return this.permissionRepository.getOneById(id);
+        return this.repository.getOneById(id);
 
     }
 
+    @Transactional
+    public Page find(Pageable pageable, SearchText searchText) {
+        return this.repository.findAll(pageable, searchText.getText());
+    }
 }
